06c15e3c30663e047921b485fe7372fa5054f12e
Kajalben <kajalsavani191@gmail.com>
2021-03-15 20:26:26 +0100

    add card

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,29 @@
+import { Grid, Container } from "@material-ui/core";
+import Card from "./Card";
 
-const Main = () =>{
-    return(
-        <h1>Main</h1>
-    );
-}
+const Main = ({ pokemons }) => {
+  console.log(pokemons);
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        <Grid
+          container
+          item
+          sm={12}
+          style={{ border: "1px solid black" }}
+          spacing={3}
+        >
+          {pokemons.map((pokemon) => {
+            return (
+              <Grid item sm={3} spacing={4}>
+                <Card pokemon={pokemon} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Main;
