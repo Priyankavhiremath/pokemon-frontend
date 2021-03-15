8e280dbb14383b35294a88e87edf8b85a10fcd8f
Tim Schneider <timjo1@MacBook-Air.fritz.box>
2021-03-15 01:48:04 +0100

    added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,17 @@ const Footer = () => {
             <div>
             <section class="mb12- align-center primarycolor">
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Pokemon" role="button"><i class="fab fa-facebook-f"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Pokemon" role="button" ><i class="fab fa-twitter"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/pokemon/" role="button"><i class="fab fa-instagram"></i ></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i ></a>
-
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/company/pokemon/" role="button"><i class="fab fa-linkedin-in"></i></a>
 
                 <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Priyankavhiremath/pokemon-frontend" role="button"><i class="fab fa-github"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-discord"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://pokewiki.de/images/c/c3/Sugimori_054.png" role="button"><i class="fab fa-discord"></i></a>
             </section>
             </div>
             <div className="text-center p-3">
