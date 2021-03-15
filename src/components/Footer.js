d2295d97bcbcae78512e06e03393dd64f5e3d596
Tim Schneider <timjo1@MacBook-Air.fritz.box>
2021-03-15 16:41:40 +0100

    center icons, image and added links again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,27 @@ const Footer = () => {
     return (
         <footer className="btn-info  text-center text-lg-start">
             <div>
-            <section class="mb12- align-center primarycolor">
+            <section class="mb12- text-center primarycolor">
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Pokemon" role="button"><i class="fab fa-facebook-f"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Pokemon" role="button" ><i class="fab fa-twitter"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/pokemon/" role="button"><i class="fab fa-instagram"></i ></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i ></a>
-
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/company/pokemon/" role="button"><i class="fab fa-linkedin-in"></i></a>
 
                 <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Priyankavhiremath/pokemon-frontend" role="button"><i class="fab fa-github"></i></a>
 
-                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-discord"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://pokewiki.de/images/c/c3/Sugimori_054.png" role="button"><i class="fab fa-discord"></i></a>
             </section>
             </div>
             <div className="text-center p-3">
-                © 2021 Copyright: PokeFight
+                © 2021 Copyright: PokeFive
              </div>
-             <div><img src="https://pokewiki.de/images/c/c3/Sugimori_054.png" width="200" /></div>
+             <div>
+                 <img src="https://pokewiki.de/images/c/c3/Sugimori_054.png" width="70" />
+                 </div>
         </footer>
     );
 };
